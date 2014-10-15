2c83e06e2403fa11e798d69ac1a1661e2962a7d4
Andrey K <andkolok@gmail.com>
2014-10-15 21:25:43 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScaffoldProject::Application.config.secret_key_base = '89b7fa935cbe38107e2fa658098b61c7030cd14ce99e3e7a8558da4262c21b36ee6ac19ebb9f3b6645950cfd730d2af433bdc825526ee4e3270f96df0f380dc1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ScaffoldProject::Application.config.session_store :cookie_store, key: '_scaffold_project_session'
